f43a7325a749738660571b2ee321ab95be3e9bda
Zain <zjabbar@hawaii.edu>
2025-06-26 14:51:19 +0000

    Ordering arguments differently and allowing for modified R covariance.

--- angles_only.m
+++ angles_only.m
@@ -1,20 +1,16 @@
 % Returns the azimuth and elevation in radians [2x1] Array
-% add_noise
-function measurements = angles_only(positions, add_noise, mu)
-    if nargin < 2
-        add_noise = false;
-    end
-    
+% R = diag([(0.5 * pi / 180)^2, (0.5 * pi / 180)^2]);
+% add_noise is either {true, false}
+function measurements = angles_only(positions, mu, R, add_noise)
     x = positions(:,1) + mu;
-    y = positions(:,2); 
+    y = positions(:,2);
     z = positions(:,3);
     
     alpha = atan2(y, x);
     epsilon = asin(z ./ sqrt(x.^2 + y.^2 + z.^2));
     measurements = [alpha epsilon];
     
-    if add_noise
-        covariance = diag([(0.5 * pi / 180)^2, (0.5 * pi / 180)^2]);
-        measurements = mvnrnd(measurements', covariance)';
+    if add_noise 
+        measurements = mvnrnd(measurements', R)';
     end
 end
