b7d96834bbb72fac1fea586e998e048e30e418b0
Zain <zjabbar@hawaii.edu>
2025-06-26 15:23:12 +0000

    Implementing Jacobian function for angles_only measurement system.

--- Test/test_engmf_update_in_loop.m
+++ Test/test_engmf_update_in_loop.m
@@ -1,12 +1,22 @@
-% Test EnGMF update with actual measurements and states from main.m
-clear; clc;
+clear
+clc
+%%
+close all
+%%
+runKDE = true;
+runGUE = false;
+runEnGMM = true;
+screenshotPlots = false;
+compileToVideo = false;
 
-% Gravitational constant (same as main.m)
+% Gravitational constant
 mu = 0.012150584269940; 
 
-% 1. Get initial points same as main.m
 nSamples = 100;
-mu_initial = [1.021339954388544 -0.000000045869005 -0.181619950369762 0.000000617839352 -0.101759879771430 0.000001049698173]';
+displaySamples = 100;
+
+% Specify initial mean for object CRTBP non-dimensionalized coordinates
+mu_initial = [1.021339954388544 -0.000000045869005 -0.181619950369762 0.000000617839352 -0.101759879771430 0.000001049698173]'; % mean.rot.t0;  % mean of initial state (in rotating frame)
 P_initial = 1.0e-08 *[ ...
    0.067741479217036  -0.000029214433641   0.000292500436172   0.000343197998120  -0.000801894296500  -0.000076851751508
   -0.000029214433641   0.067949657828148  -0.000045655889447   0.000112485276059   0.002893878948354  -0.000038999497288
@@ -17,81 +27,27 @@ P_initial = 1.0e-08 *[ ...
 
 ICs = mvnrnd(mu_initial, P_initial, nSamples);
 
-% 2. Propagate points like in main.m
 measurementInterval = 0.005;
 frameInterval = 0.001;
+
+% set up initial time and final time (non dimensional)
 t0 = 0;      
 tf = 0.75103;
 
+% Initialize Condition is Gaussian
+nX     = 6;  
+w0     = {1};
+m0     = {mu_initial};
+P0     = {P_initial};         
+dH = 1.455021851351014; 
 [t, Y] = PropagateSamples(ICs, t0, frameInterval, measurementInterval);
 
-% 3. Run EnGMF update on angles_only measurement
-% Get final propagated states
-Y_final = Y(:,:,end);  % nSamples x 6 matrix
-
-% Measurement covariance for angles-only (azimuth, elevation)
-angle_std = 0.5 * pi / 180;  % 0.5 degrees in radians
-R = diag([angle_std^2, angle_std^2]);
+% Measurement Covariance - R
+R = diag([(0.5 * pi / 180)^2, (0.5 * pi / 180)^2]);
+% Measurements num_samples 
+Z = angles_only(Y(:,:,end), mu, R, tre);
 
 
-% Create angles-only measurement (azimuth, elevation)
-Z = angles_only(Y_final, mu, R, true);  % Returns measurements with noise
 
-% Set up for EnGMF update
-% Convert samples to the format expected by engmf_update
-m_predict = Y_final';  % 6 x nSamples (each column is a state vector)
 
-% Create uniform prior covariances using KDE bandwidth
-state_dim = 6;
-sample_mean = mean(Y_final, 1);
-centered_samples = Y_final - sample_mean;
-sample_cov = (centered_samples' * centered_samples) / (nSamples - 1);
 
-% Silverman's rule for bandwidth
-beta_silv = (4 / (nSamples * (state_dim + 2)))^(2 / (state_dim + 4));
-B = beta_silv * sample_cov;
-
-% Create covariance array (6 x 6 x nSamples)
-P_predict = repmat(B, [1, 1, nSamples]);
-
-% Measurement covariance for angles-only (azimuth, elevation)
-angle_std = 0.5 * pi / 180;  % 0.5 degrees in radians
-R = diag([angle_std^2, angle_std^2]);
-
-% Set up configuration and model for measurement function
-cfig = Config();
-model.rIs = zeros(3,1);  % sensor position
-model.vIs = zeros(3,1);  % sensor velocity
-
-% Run EnGMF update for each measurement
-num_measurements = size(Z, 1);
-weights_out = cell(num_measurements, 1);
-means_out = cell(num_measurements, 1);
-covs_out = cell(num_measurements, 1);
-
-for i = 1:num_measurements
-    y = Z(i, :)';  % Current measurement [azimuth; elevation]
-    
-    % Run the EKF-style update for each particle (same as run_filter.m)
-    [m_update, P_update, U_update] = update(cfig, model, y, R, m_predict, P_predict);
-    
-    % Compute weights using log-sum-exp for numerical stability
-    log_weights = U_update - max(U_update);
-    weights = exp(log_weights);
-    weights = weights / sum(weights);  % Normalize
-    
-    % Store results
-    weights_out{i} = weights;
-    means_out{i} = m_update;
-    covs_out{i} = P_update;
-    
-    fprintf('Measurement %d: Updated %d components, total weight = %.4f\n', ...
-            i, length(weights), sum(weights));
-end
-
-% Display summary
-fprintf('\nEnGMF Update Complete:\n');
-fprintf('- Processed %d measurements\n', num_measurements);
-fprintf('- Each update returned %d weighted components\n', nSamples);
-fprintf('- Final mean position: [%.6f, %.6f, %.6f]\n', ...
-        mean(means_out{end}(1:3,:), 2));
--- engmf_update.m
+++ engmf_update.m
@@ -5,7 +5,7 @@ function [m_update, P_update, w_update] = engmf_update(h_func, H_func, z, m_pred
     
     cfig.h = h_func;
     cfig.H = H_func;
-    model.D = size(m_predict, 1);
+    % model.D = size(m_predict, 1);
     model.R = R;
     model.P_D = 1;
     model.lambda_c = 0;
@@ -68,29 +68,3 @@ function [x_out, P_out, w_out] = gen_gms_kdesilv(model,w,m,P,num_par)
 %
 end
 
-function [m_update,P_update,U_update] = update(cfig,model,y,R,m,P)
-%      
-    U_update = zeros(size(m,2),1);
-    m_update = zeros(size(m,1),size(m,2));
-    P_update = zeros(size(m,1),size(m,1),size(m,2));
-    for jj=1:size(m,2)
-        % individual EKF update
-        ybar = cfig.h(model,m(:,jj),'noiseless');
-        Hj   = cfig.H(model,m(:,jj));
-        Pxxj = P(:,:,jj);
-        Pxyj = Pxxj * Hj';
-        Pyyj= Hj*Pxxj*Hj' + R; 
-        Pyyj= (Pyyj + Pyyj')/2;   % additional step to avoid numerical problems
-        det_Pyyj = prod(eig(Pyyj)); iPyyj = pinv(Pyyj);
-        Kj = Pxyj * iPyyj;
-        m_update(:,jj)  = m(:,jj) + Kj*(y-ybar);
-        Ij = eye(size(m,1));
-        P_update(:,:,jj) = (Ij - Kj*Hj) * Pxxj * (Ij - Kj*Hj)' + Kj * R * Kj'; % Joseph form
-        
-        % weight update
-        U_update(jj) = -(y-ybar)' * (iPyyj * (y-ybar)) / 2 ...
-                       - log(det_Pyyj) / 2 ...
-                       - log(2*pi) * size(y,1) / 2;
-    end
-%
-end
