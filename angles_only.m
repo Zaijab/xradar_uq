ff41dc0c519e97f22882cf9a1d11a1803c2f0288
Zain <zjabbar@hawaii.edu>
2025-06-26 13:08:37 +0000

    Adding utility functions.
    Measurement function angles_only
    Points -> GMM via Silverman estimates in kde_silverman

---
+++ angles_only.m
@@ -0,0 +1,20 @@
+% Returns the azimuth and elevation in radians [2x1] Array
+% add_noise
+function measurements = angles_only(positions, add_noise)
+    if nargin < 2
+        add_noise = false;
+    end
+    
+    x = positions(1);
+    y = positions(2); 
+    z = positions(3);
+    
+    alpha = atan2(y, x);
+    epsilon = asin(z / sqrt(x^2 + y^2 + z^2));
+    measurements = [alpha; epsilon];
+    
+    if add_noise
+        covariance = diag([(0.5 * pi / 180)^2, (0.5 * pi / 180)^2]);
+        measurements = mvnrnd(measurements', covariance)';
+    end
+end
--- kde_silverman.m
+++ kde_silverman.m
@@ -1,31 +1,24 @@
-% Generate Gaussian Mixture Samples using Kernel Density Estimation and
-% Silverman's Rule of Thumb
-function [x_out, P_out, w_out] = gen_gms_kdesilv(model,w,m,P,num_par)
-%
-    if num_par > 0
-        u = rand(model.stream,[num_par,1]);
-        wcumsum = cumsum(w/sum(w));
-        x_out = zeros(model.x_dim,num_par);
-        P_out = zeros(model.x_dim,model.x_dim,num_par);
-        for ii = 1:num_par
-            idx = find(wcumsum >= u(ii), 1, 'first');
-            sqrt_Pk = sqrtm(P(:,:,idx));
-            x_out(:,ii)   = m(:,idx) + sqrt_Pk * randn(model.stream, [model.x_dim,1]) ;
-            P_out(:,:,ii) = P(:,:,idx); % if sampling from set, then return component's covariance
-        end
-        w_out = sum(w) * ones(num_par,1) ./ num_par; % uniform weights
-        betaS_scale = 1; % Silverman's rule of thumb heuristic scaling coefficient (>1 more conservative / <1 more confident)
-        if num_par > 1
-            betaS = (betaS_scale/ceil(sum(w_out)))*(4/(num_par*(model.x_dim+2))) ^ (2/(model.x_dim+4)); % Silverman's rule of thumb
-            mux = mean(x_out,2); ex = x_out - mux; P = betaS * ((ex * ex') / (num_par-1) ); % Pbar (sample covariance)
-            P_out = repmat(P,1,1,num_par);
-        else
-            P_out = [];
-        end
-    else
-        x_out = [];
-        P_out = [];
-        w_out = [];
-    end
-%
+% Example
+% gm = gmdistribution([1,2; 0,0], cat(3, eye(2), eye(2)))
+% points = random(gm, 10)
+% new_gm = kde_silverman(points)
+% new_gm.pdf([1,2])
+
+function gm = kde_silverman(points)
+    %
+    
+    [n, d] = size(points);  % n points in d dimensions
+    assert(n > 1, 'Need multiple points');
+    
+    % Silverman's rule of thumb
+    beta = (4/(n*(d+2)))^(2/(d+4));
+    sample_cov = cov(points);
+    P = beta * sample_cov;
+    
+    % Each point becomes a component with uniform weight
+    mu = points;  % points is already n x d
+    sigma = repmat(P, [1 1 n]);
+    weights = ones(1,n) / n;
+    
+    gm = gmdistribution(mu, sigma, weights);
 end
